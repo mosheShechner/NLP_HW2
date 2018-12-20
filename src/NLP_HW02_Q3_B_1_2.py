4a0d6ff10a2030558bab23102d778d38cf7f43b6
mosheShechner <moshe.shechner@gmail.com>
2018-12-20 20:25:57 +0200

    bullet 3.1.2 ready, not running due to technical issue with imports run on jupyter notebook

--- src/NLP_HW02_Q3_B_1_2.py
+++ src/NLP_HW02_Q3_B_1_2.py
@@ -2,10 +2,11 @@
 # module        NLP_HW02_Q3_B_1_2
 # ##########################################################################################
 # path          ...\NLP_HW02\repository\src\NLP_HW02_Q3_B_1_2.py
-# Purpose       Executions of bullet 3.1.1
+# Purpose       Executions of bullet 3.1.2
 # description
 # ##########################################################################################
 import pycrfsuite
+import scipy
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 from nltk.corpus import conll2002
@@ -76,6 +77,14 @@ print("Correct:  ", ' '.join(fe.getSentY(example_sent)))
 print("-> predicting test data'eta'")
 Y_pred      = tagger.tag(X_test)
 
+print("real tag list # %d;" % len(Y_test))
+print("pred tag list # %d;" % len(Y_pred))
+test_class_names = set(Y_test)
+pred_class_names = set(Y_pred)
+print(test_class_names)
+print(pred_class_names)
+
+
 print("printing few results")
 
 print("token    : %s" % token_test[0])
@@ -87,6 +96,7 @@ print("token    : %s" % token_test[1])
 print("x        : %s" % X_test[1])
 print("real     : %s" % Y_test[1])
 print("predicted: %s" % Y_pred[1])
+
 
 def bio_classification_report(y_true, y_pred):
     """
@@ -111,4 +121,49 @@ def bio_classification_report(y_true, y_pred):
         target_names=tagset,
     )
 
-print(bio_classification_report(Y_test, Y_pred))
+
+def measureModel(train, test, order, experimentName="defExperimentName", verbose=False):
+    # encode training data
+    if verbose: print("-> encoding train data")
+    X_train = fe.getX(train, order)
+    Y_train = fe.getY(train)
+
+    # train a classifier
+    if verbose: print("-> setting trainer parameters")
+    trainer = pycrfsuite.Trainer(verbose=False)
+    trainer.set_params({
+        'c1': 1.0,  # coefficient for L1 penalty
+        'c2': 1e-3,  # coefficient for L2 penalty
+        'max_iterations': 50,  # stop earlier
+        # include transitions that are possible, but not observed
+        'feature.possible_transitions': True
+    })
+    if verbose: print("-> setting data to trainer")
+    trainer.append(X_train, Y_train)
+    if verbose: print("-> training; classifier saved on ./%s" % experimentName)
+    trainer.train(experimentName)
+
+    # encode test data
+    if verbose: print("-> encoding test data")
+    X_test = fe.getX(test, order)
+    Y_test = fe.getY(test)
+
+    # tag test data (prediction)
+    if verbose: print("-> tagging test data with trained classifier %s" % experimentName)
+    tagger = pycrfsuite.Tagger()
+    tagger.open(experimentName)
+    Y_pred = tagger.tag(X_test)
+
+    # print peasured results:
+    print("\n\nModel performance classification results for experiment: %s" % experimentName)
+    print(bio_classification_report(Y_test, Y_pred))
+
+
+# measure models order={0,1,2} for {Spanish, Dutch}
+measureModel(etr, eta, 0, experimentName="Lang=Spanish_X_Order=0", verbose=False)
+measureModel(etr, eta, 1, experimentName="Lang=Spanish_X_Order=1", verbose=False)
+measureModel(etr, eta, 2, experimentName="Lang=Spanish_X_Order=2", verbose=False)
+
+measureModel(dtr, dta, 0, experimentName="Lang=Dutch_X_Order=0", verbose=False)
+measureModel(dtr, dta, 1, experimentName="Lang=Dutch_X_Order=1", verbose=False)
+measureModel(dtr, dta, 2, experimentName="Lang=Dutch_X_Order=2", verbose=False)
